98b8a413a0693ee862a010b08ce88fa95934b024
Goba lKrishnan V <39051061+gobalkrishnanv@users.noreply.github.com>
2018-12-07 18:01:02 +0530

    Add files via upload

---
+++ Country.java
@@ -0,0 +1,156 @@
+
+public class Country {
+
+public static final String afghanistan="afghanistan";
+public static final String albania="albania";
+public static final String algeria="algeria";
+public static final String andorra="andorra";
+public static final String angola="angola";
+public static final String antigua_and_barbuda="antigua and barbuda";
+public static final String argentina="argentina";
+public static final String armenia="armenia";
+public static final String australia="australia";
+public static final String austria="austria";
+public static final String azerbaijan="azerbaijan";
+
+
+public static final String bahamas="bahamas";
+public static final String bahrain="bahrain";
+public static final String bangladesh="bangladesh";
+public static final String barbados="barbados";
+public static final String belarus="belarus";
+public static final String belgium="belgium";
+public static final String belize="belize";
+public static final String benin="benin";
+public static final String bhutan="bhutan";
+public static final String bolivia="bolivia";
+public static final String bonsia_and_Herzegovina="Bosnia and Herzegovina";
+public static final String botswana="botswana";
+public static final String brazil="brazil";
+public static final String brunei="brunei"; 
+public static final String bulgaria="bulgaria";
+public static final String burkina_faso="burkina faso";
+public static final String burundi="burundi";
+
+public static final String cabo_verde="Carbo Verde";
+public static final String cambodia="cambodia";
+public static final String cameroon="cameroon";
+public static final String canada="canada";
+public static final String central_african_republic="central african republic";
+public static final String chad="chad";
+public static final String chile="chile";
+public static final String china="china";
+public static final String colombia="colombia";
+public static final String comoros="comoros";
+public static final String democratic_republic_congo="democratic republic congo";
+public static final String republic_congo="republic congo";
+public static final String costa_rica="costa rica";
+public static final String cote_d_Ivoire="cote d'Ivoire";
+public static final String croatia="croatia";
+public static final String cuba="cuba";
+public static final String cyprus="cyprus";
+public static final String czech_republic="czech republic";
+
+public static final String denmark="denmark";
+public static final String djibouti="djibouti";
+public static final String dominica="dominica";
+public static final String dominican_republic="dominican republic";
+
+public static final String ecuador="ecuador";
+public static final String egypt="egypt";
+public static final String ei_salvador="ei salvador";
+public static final String equatorial_guinea="equatorial_guinea";
+public static final String eritrea="eritrea";
+public static final String estonia="estonia";
+public static final String eswatini="eswatini";
+public static final String ethiopia="ethiopia";
+
+public static final String fiji="fiji";
+public static final String finland="finland";
+public static final String france="france";
+
+public static final String gabon="gabon";
+public static final String gambia="gambia";
+public static final String georgia="georgia";
+public static final String germany="germany";
+public static final String ghana="ghana";
+public static final String greece="greece";
+public static final String grenada="grenada";
+public static final String guatemala="guatemala";
+public static final String guinea="guinea";
+public static final String guinea_bissau="guinea-bissau";
+public static final String guyana="guyana";
+
+public static final String haiti="haiti";
+public static final String honduras="honduras";
+public static final String hungary="hungary";
+
+public static final String iceland="iceland";
+public static final String india="india";
+public static final String indonesia="indonesia";
+public static final String iran="iran";
+public static final String iraq="iraq";
+public static final String ireland="ireland";
+public static final String israel="israel";
+public static final String italy="italy";
+
+
+public static final String jamaica="jamaica";
+public static final String japan="japan";
+public static final String jordan="jordan";
+
+
+public static final String kazakhstan="kazakhstan";
+public static final String kenya="kenya";
+public static final String kiribati="kiribati";
+public static final String kosovo="kosovo";
+public static final String kuwait="kuwait";
+public static final String kyrgyzstan="kyrgzstan";
+
+public static final String laos="laos";
+public static final String latvia="latvia";
+public static final String lebanon="lebanon";
+public static final String lesotho="lesotho";
+public static final String liberia="liberia";
+public static final String libya="libya";
+public static final String liechtenstein="liechtenstein";
+public static final String lithuania="lithuania";
+public static final String luxembourg="luxembourg";
+
+public static final String macedonia="macedonia";
+public static final String madagascar="madagascar";
+public static final String malawi="malawi";
+public static final String malaysia="malaysia";
+public static final String maldives="maldives";
+public static final String mali="mali";
+public static final String malta="malta";
+public static final String marshall_island="marshal_island";
+public static final String mauritania="mauritania";
+public static final String mauritius="mauritius";
+public static final String mexico="mexico";
+public static final String micronesia="micronesia";
+public static final String moldova="moldova";
+public static final String monaco="monaco";
+public static final String mongolia="mongolia";
+public static final String montengro="montengro";
+public static final String mozambique="monzambique";
+public static final String myanmar="myanmar";
+
+public static final String namibia="namibia";
+public static final String nauru="nauru";
+public static final String nepal="nepal";
+public static final String netherlands="netherlands";
+public static final String new_zealand="new zealand";
+public static final String nicaragua="nicaragua";
+public static final String niger="niger";
+public static final String nigeria="nigeria";
+public static final String north_korea="north_korea";
+public static final String norway="norway";
+
+public static final String oman="oman";
+
+public static final String pakistan="pakistan";
+
+
+
+}
